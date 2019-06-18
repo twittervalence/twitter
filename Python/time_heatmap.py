f5c21f879c3146607c1d5ade49f40e419c5d3248
bayoishola20 <bayoishola20@yahoo.com>
2019-06-18 19:45:33 +0200

    Python

--- Python/time_heatmap.py
+++ Python/time_heatmap.py
@@ -3,18 +3,18 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 
-data = pd.read_csv('../Python/ExtractedCSV/Munich_tweet_time.csv')
+data = pd.read_csv('../Python/ExtractedCSV/Munich_tweet_time.csv') #choose file to process
 
 #print(data)
 
 
 
 
-tweets_date_hour = data.pivot_table(index=["year"], columns="month", values = "polarity", aggfunc='mean')
+tweets_date_hour = data.pivot_table(index=["year"], columns="month", values = "polarity", aggfunc='mean') #change for subjectivity and polarity attributes
 f, ax = plt.subplots(figsize = (12,5))
 sns.heatmap(tweets_date_hour, linewidths=.5, cmap="YlGnBu", annot=True)
-ax.set_title('Munich Tweet Valence Heatmap')
-ax.set_xticklabels(['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'])
+ax.set_title('Munich Tweet Valence Heatmap') #set plot title
+ax.set_xticklabels(['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']) # set x-axis label
 sns.set(font_scale=20)
 # plt.show()
-plt.savefig('../Python/TimeHeatMap/Munich_tweet_time.png')
+plt.savefig('../Python/TimeHeatMap/Munich_tweet_time.png') #save file: change name here
